6affead8b0412e8e80e8ee8cf8e3d530d55a34e4
Taylor Sam <taylorsam209@yahoo.com>
2017-11-03 12:26:21 -0600

    fixed response

--- app/question.service.js
+++ app/question.service.js
@@ -2,7 +2,7 @@ angular.module("triviaApp").service('questionSrvc', function($http) {
 
     this.getQuestions = function() {
         return $http.get("https://practiceapi.devmountain.com/api/trivia/questions").then(response=> {
-            return response
+            return response.data
         }).catch(err=>{console.log("Error in questions", err)})
     }
 
