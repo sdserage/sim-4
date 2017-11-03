0c7c5cc35d7ab1bc1a335da7dee494f032eeaed1
Taylor Sam <taylorsam209@yahoo.com>
2017-11-03 12:07:20 -0600

    create delete endpoint

--- app/question.service.js
+++ app/question.service.js
@@ -5,4 +5,11 @@ angular.module("triviaApp").service('questionSrvc', function($http) {
             return response
         }).catch(err=>{console.log("Error in questions", err)})
     }
+
+    this.deleteQuestion = function(id) {
+        return $http.delete("https://practiceapi.devmountain.com/api/trivia/questions/:" + id).then(response => {
+            return "Delete Successful"
+        }).catch( err => {console.log("error in delete", err)})
+    }
+
 })
