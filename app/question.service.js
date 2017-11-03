5c1cf6921738fbccbbe685b503a8ff439d95bad0
Taylor Sam <taylorsam209@yahoo.com>
2017-11-03 13:57:55 -0600

    added openModal

--- app/question.service.js
+++ app/question.service.js
@@ -6,6 +6,12 @@ angular.module("triviaApp").service('questionSrvc', function($http) {
         }).catch(err=>{console.log("Error in questions", err)})
     }
 
+    this.getQuestion = function(id) {
+        return $http.get("https://practiceapi.devmountain.com/api/trivia/questions/:" + id).then(response=> {
+            return response.data
+        }).catch(err=> {console.log("Error in single question", err)})            
+    }
+
     this.deleteQuestion = function(id) {
         return $http.delete("https://practiceapi.devmountain.com/api/trivia/questions/:" + id).then(response => {
             return "Delete Successful"
--- app/questionCrtl.js
+++ app/questionCrtl.js
@@ -1,5 +1,20 @@
-angular.module("triviaApp").controller("questionCtrl", function($scope, questionSrvc) {
+angular.module("triviaApp").controller("questionCtrl", function($scope, $stateParams, questionSrvc) {
+    
+    $scope.modalOpen = false;
+    $scope.id = $stateParams.id;
+
     questionSrvc.getQuestions().then(questions => {
         $scope.questions = questions;
     })
+
+    $scope.openModal= function(question){
+        $scope.modalOpen = true;
+        if(question) {
+        questionSrvc.getQuestion($stateParams.id).then(question => {
+            $scope.currentQuestion = question;
+    })
+        }else{
+            return null;
+        }
+    }
 })
